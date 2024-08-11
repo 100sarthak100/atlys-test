5f6394de2ecf6421cac13c104eef8c0869a33a5f
Sarthak Naithani <sarthaknaithani127@gmail.com>
2024-08-11 19:07:37 +0530

    Added auth logo

--- src/components/AuthWrapper/AuthWrapper.tsx
+++ src/components/AuthWrapper/AuthWrapper.tsx
@@ -4,7 +4,11 @@ const AuthWrapper: React.FC<{ children: ReactNode }> = (props) => {
   const { children } = props;
 
   return (
-    <div className="flex w-full min-h-screen justify-center items-center bg-mainBg">
+    <div className="flex flex-col w-full min-h-screen justify-center items-center bg-mainBg">
+      <div className="mb-[49px]">
+        <img src="Logo.svg" />
+      </div>
+
       {children}
     </div>
   );
